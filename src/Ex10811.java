9a736636fb18b5529f2bacfe8a8f0bc06ced56a9
wlgns <wlgns5510@jitpower.com>
2023-11-24 16:27:05 +0900

    깃 테스트2

---
+++ src/Ex10811.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex10811 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //바구니의 개수
+        int N = sc.nextInt();
+
+        System.out.println("바구니의 개수: " + N);
+
+        sc.close();
+    }
+}
